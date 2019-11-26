78221bca64cb2b84a4844874aabcf090200a798f
Jacob Rienstra <jacob@Sheikah-Slate.local>
2019-11-26 01:20:09 -0500

    test.js added to migrate, first gatsby skeleton

---
+++ src/test.js
@@ -0,0 +1,11 @@
+const { SDK } = require('@directus/sdk-js');
+
+const test = async () => {
+  const options = {
+    url: 'http://api.satirev.org',
+    project: 'satire-v',
+    token: 'letmeinyoubitch',
+  };
+  const sdk = new SDK(options);
+};
+test();
